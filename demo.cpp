1c2db597ec8f2df260098b1e53bfe89e632ccc06
ishant <ishantbhoyar59@gmail.com>
2026-01-16 13:51:54 +0530

    Day1 of c++

---
+++ demo.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    cout << "hello from windows";
+    return 0;
+}
---
+++ newlin2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    cout << "hello";
+    cout << "\n";
+    cout << "ishant" << endl;
+    cout << "from windows";
+    return 0;
+}
