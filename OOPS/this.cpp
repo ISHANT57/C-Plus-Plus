b6e8ce012c195a6c9cc74b724c663c239d6fecec
ishant <ishantbhoyar59@gmail.com>
2026-01-17 15:51:38 +0530

    usse of this

---
+++ OOPS/this.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+    int age;
+public:
+    void setAge(int age) {
+        this->age = age;   // this->age = object variable
+    }
+
+    void show() {
+        cout << "Age: " << age;
+    }
+};
+
+int main() {
+    Student s1;
+    s1.setAge(20);
+    s1.show();
+}
